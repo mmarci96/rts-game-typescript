187447050c467a6e7d0476820f6a953d5bcb9b83
mmarci96 <sarosdimarci@gmail.com>
2025-02-19 22:46:50 +0100

    my tests did not work for maps... but i see it in the browser so sadly the issue is in the test...:c

--- game/apps/game-api/src/api/index.ts
+++ game/apps/game-api/src/api/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes";
 import gameRoutes from "./routes/game.routes";
+import mapRoutes from "./routes/map.routes";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/games", gameRoutes);
+router.use("/maps", mapRoutes);
 
 export default router;
