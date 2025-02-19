f88601bf5fc472317764b2f359cb5b9c7be1149d
mmarci96 <sarosdimarci@gmail.com>
2025-02-19 12:01:01 +0100

    i found what i was looking for

--- game/apps/game-client/src/components/common/PopupCard.tsx
+++ game/apps/game-client/src/components/common/PopupCard.tsx
@@ -12,7 +12,7 @@ interface PopupCardProps {
 const PopupCard = ({ header, children, footer, onClose }: PopupCardProps) => {
     return (
         <>
-            <div className="brightness-50  bg-transparent w-screen h-screen z-10 absolute"></div>
+            <div className=" backdrop-blur-xs w-screen h-screen z-10 absolute"></div>
             <div
                 className="fixed inset-0 flex items-center z-20 justify-center"
                 onClick={onClose}
