0db1e5c8f066b92f4fd803c828f52fa391f2b1c4
mmarci96 <sarosdimarci@gmail.com>
2025-02-19 22:15:13 +0100

    forgot to actually add the route

--- game/apps/game-api/src/api/index.ts
+++ game/apps/game-api/src/api/index.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import authRoutes from './routes/auth.routes';
+import express from "express";
+import authRoutes from "./routes/auth.routes";
+import gameRoutes from "./routes/game.routes";
 
 const router = express.Router();
 
-router.use('/auth', authRoutes)
-
+router.use("/auth", authRoutes);
+router.use("/games", gameRoutes);
 
 export default router;
