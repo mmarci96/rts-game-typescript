d1bdd3fe42c7cb5261f3e627300a692fa6ec3139
mmarci96 <sarosdimarci@gmail.com>
2025-02-19 14:28:52 +0100

    unit generator functions added but ran into some reference issues on player-game relation

---
+++ game/apps/game-api/src/service/map.service.ts
@@ -0,0 +1,20 @@
+import { MapModel } from "@packages/game-db"
+import { Types } from "mongoose";
+
+export const getMaps = async () => {
+    const maps = await MapModel.find().select('_id type size');
+    if (!maps) {
+        throw new Error("No maps found!");
+    }
+
+    return maps;
+}
+
+export const getMapById = async (mapId: Types.ObjectId) => {
+    const map = await MapModel.findById(mapId);
+    if (!map) {
+        throw new Error("Map not found with id: " + mapId.toString());
+    }
+
+    return map;
+}
