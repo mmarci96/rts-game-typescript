ed5dc484a3a2bd1b8eb821655562c594042b9ac5
mmarci96 <sarosdimarci@gmail.com>
2025-02-23 03:41:00 +0100

    the import still off on react, but i need to start the game and im good to go

--- game/apps/game-client/vite.config.ts
+++ game/apps/game-client/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss(),],
+    plugins: [react(), tailwindcss()],
     server: {
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         port: 5173,
         proxy: {
-            '/api': {
-                target: 'http://localhost:5000',
+            "/api": {
+                target: "http://localhost:5000",
                 changeOrigin: true,
                 secure: false,
             },
         },
     },
-})
+});
