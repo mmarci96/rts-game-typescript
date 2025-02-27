0923b979bbf3a1bf443faa6ee06c85a28b524bbf
mmarci96 <sarosdimarci@gmail.com>
2025-02-27 18:53:37 +0100

    added handler for socket

--- game/apps/game-ui/src/main.ts
+++ game/apps/game-ui/src/main.ts
@@ -1,6 +1,19 @@
-import './style.css'
+import { io, Socket } from 'socket.io-client';
+
+const socketHandler = (socket: Socket) => {
+    socket.on('connect', () => {
+        console.log("connected");
+    })
+}
 
 const loadEvent = async () => {
+    document.addEventListener('contextmenu', e => e.preventDefault());
+    const url = window.location.pathname;
+    console.log(url);
+    const socket = io();
+    socketHandler(socket);
+
+
     console.log("Hello world!")
 }
 
