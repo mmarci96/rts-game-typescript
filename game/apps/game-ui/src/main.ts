6cee66b81a828b5862aaf6ddb1fae0d850e912ad
mmarci96 <sarosdimarci@gmail.com>
2025-02-27 19:28:04 +0100

    gameid and userid parsed from url, after this send to websocket the ui init is rdy

--- game/apps/game-ui/src/main.ts
+++ game/apps/game-ui/src/main.ts
@@ -6,10 +6,23 @@ const socketHandler = (socket: Socket) => {
     })
 }
 
+
+const getIdFromUrl = (url: string) => {
+    const arr = url.split("/");
+    const lastIndex = arr.length - 1
+
+    return {
+        gameId: arr[lastIndex - 1],
+        userId: arr[lastIndex]
+    }
+}
 const loadEvent = async () => {
     document.addEventListener('contextmenu', e => e.preventDefault());
     const url = window.location.pathname;
-    console.log(url);
+    const { gameId, userId } = getIdFromUrl(url);
+
+    console.log("gameId", gameId);
+    console.log("userid", userId);
     const socket = io();
     socketHandler(socket);
 
