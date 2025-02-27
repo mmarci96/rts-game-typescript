58dbba0115717c6018188eb183eab269d4bd2820
mmarci96 <sarosdimarci@gmail.com>
2025-02-27 19:37:39 +0100

    gameid and playerid succesfully arriving on connection through websocket

--- game/apps/game-server/src/ws/index.ts
+++ game/apps/game-server/src/ws/index.ts
@@ -3,6 +3,13 @@ import { Server, Socket } from "socket.io";
 export const websocketController = (io: Server) => {
     io.on('connection', (socket: Socket) => {
         console.log("New connection: ", socket.id);
+        socket.on("load_game", (data) => {
+            const { playerId, gameId } = data;
+            console.log("playerId", playerId);
+            console.log('gameid', gameId);
+
+        })
+
 
         socket.on('disconnect', () => {
             console.log('Connection ended: ', socket.id)
--- game/apps/game-ui/src/main.ts
+++ game/apps/game-ui/src/main.ts
@@ -1,8 +1,10 @@
 import { io, Socket } from 'socket.io-client';
 
-const socketHandler = (socket: Socket) => {
+const socketHandler = (socket: Socket, playerId: string, gameId: string) => {
     socket.on('connect', () => {
         console.log("connected");
+        const data = { playerId, gameId };
+        socket.emit('load_game', data);
     })
 }
 
@@ -13,21 +15,19 @@ const getIdFromUrl = (url: string) => {
 
     return {
         gameId: arr[lastIndex - 1],
-        userId: arr[lastIndex]
+        playerId: arr[lastIndex]
     }
 }
 const loadEvent = async () => {
     document.addEventListener('contextmenu', e => e.preventDefault());
     const url = window.location.pathname;
-    const { gameId, userId } = getIdFromUrl(url);
+    const { gameId, playerId } = getIdFromUrl(url);
 
     console.log("gameId", gameId);
-    console.log("userid", userId);
+    console.log("userid", playerId);
     const socket = io();
-    socketHandler(socket);
+    socketHandler(socket, playerId, gameId);
 
-
-    console.log("Hello world!")
 }
 
 window.addEventListener("load", loadEvent);
