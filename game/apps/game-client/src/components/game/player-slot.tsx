19e4e20abb617ff7453619f2cf425b994fac07d5
mmarci96 <sarosdimarci@gmail.com>
2025-03-05 23:43:08 +0100

    player doesnt see join slot when joined already

--- game/apps/game-client/src/components/game/player-slot.tsx
+++ game/apps/game-client/src/components/game/player-slot.tsx
@@ -7,7 +7,7 @@ interface PlayerSlotParams {
 
 export const PlayerSlot = ({ player, onClickJoin }: PlayerSlotParams) => {
     return (
-        <div className="w-40 h-28 bg-[#11101B] p-4 m-2 rounded-2xl">
+        <div className="w-[240px] bg-[#11101B] p-4  rounded-2xl">
             {player ? (
                 <div className="flex flex-col">
                     <p className="p-2"> Player: {player.name} </p>
