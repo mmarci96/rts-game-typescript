250c967f8a3a685d71d67b328a1594ce27b3a427
mmarci96 <sarosdimarci@gmail.com>
2025-03-06 23:11:09 +0100

    unused vars and indent, after it broke on package sync

--- game/apps/game-api/src/service/map.service.ts
+++ game/apps/game-api/src/service/map.service.ts
@@ -1,14 +1,14 @@
-import { MapModel } from "@packages/game-db"
+import { MapModel } from "@packages/game-db";
 import { Types } from "mongoose";
 
 export const getMaps = async () => {
-    const maps = await MapModel.find().select('_id type size');
+    const maps = await MapModel.find().select("_id type size");
     if (!maps) {
         throw new Error("No maps found!");
     }
 
     return maps;
-}
+};
 
 export const getMapById = async (mapId: Types.ObjectId) => {
     const map = await MapModel.findById(mapId);
@@ -17,4 +17,4 @@ export const getMapById = async (mapId: Types.ObjectId) => {
     }
 
     return map;
-}
+};
