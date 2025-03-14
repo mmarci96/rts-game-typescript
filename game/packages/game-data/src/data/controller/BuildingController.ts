539fde362270ef7085e8492143111471a012e50e
mmarci96 <sarosdimarci@gmail.com>
2025-03-14 10:50:20 +0100

    buildings also need to be destroyed to end game

--- game/packages/game-data/src/data/controller/BuildingController.ts
+++ game/packages/game-data/src/data/controller/BuildingController.ts
@@ -1,5 +1,5 @@
 import { Building, MainBuilding } from "../entities";
-import { BuildingData } from "../types";
+import { BuildingData, PlayerColor } from "../types";
 import { mapBuildingToBuildingParams } from "../utils";
 
 class BuildingController {
@@ -22,9 +22,11 @@ class BuildingController {
             }
         });
     }
+
     getBuildings() {
         return [...this.#buildings.values()];
     }
+
     refreshBuilding(deltaTime: number) {
         [...this.#buildings.values()].forEach((building: Building) => {
             if (building instanceof MainBuilding) {
@@ -32,8 +34,22 @@ class BuildingController {
             }
         });
     }
+
     getBuildingById(buildingId: string) {
         return this.#buildings.get(buildingId);
+    }
+
+    checkWinner(): PlayerColor | undefined {
+        const colorPresence = new Set<PlayerColor>();
+        for (const building of this.#buildings.values()) {
+            if (building.attackable.getHealth() > 0) {
+                colorPresence.add(building.getColor());
+            }
+        }
+        if (colorPresence.size === 1) {
+            return colorPresence.values().next().value;
+        }
+        return undefined;
     }
 }
 
