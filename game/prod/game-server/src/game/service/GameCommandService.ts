15ee13b5ed944e1a737172861d02c63fca11fee5
mmarci96 <sarosdimarci@gmail.com>
2025-03-19 21:06:35 +0100

    remove prod experiment

--- game/prod/game-server/src/game/service/GameCommandService.ts
+++
@@ -1,8 +0,0 @@
-import Game from "../Game";
-import { Player } from "@packages/game-data";
-
-export class GameCommandService {
-    handlePlayerCommands(game: Game, commands: any[], player: Player): void {
-        game.getLogic().handlePlayerCommands(commands, player);
-    }
-}
