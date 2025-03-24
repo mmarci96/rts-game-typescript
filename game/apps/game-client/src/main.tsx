7608c993337071add04d76e31892a428b2cda82c
mmarci96 <sarosdimarci@gmail.com>
2025-03-24 19:11:14 +0100

    build works, need to adjust setup so it works properly when running inside docker container

--- game/apps/game-client/src/main.tsx
+++ game/apps/game-client/src/main.tsx
@@ -10,7 +10,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/home">
+        <BrowserRouter basename="/game-client">
             <App />
         </BrowserRouter>
     </StrictMode>,
