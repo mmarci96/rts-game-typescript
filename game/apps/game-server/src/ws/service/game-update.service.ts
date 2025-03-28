9bf63583ac640fdf98d7968198f7f077b60a1436
mmarci96 <sarosdimarci@gmail.com>
2025-03-28 20:54:50 +0100

    added logic to clear intervals or start them if no game is present or new one added.

--- game/apps/game-server/src/ws/service/game-update.service.ts
+++ game/apps/game-server/src/ws/service/game-update.service.ts
@@ -11,16 +11,19 @@ import {
 } from "../../redis";
 import { SaveGameStateParams } from "../../types";
 import { ConnectionService } from "./connection.service";
+import { setWinnerOnGameOver } from "@packages/game-db";
 
 export class GameUpdateService {
     private activeGames: Map<string, Game> = new Map();
     private updateInterval: NodeJS.Timeout | null = null;
+    private io: Server;
 
     constructor(io: Server) {
-        this.startGameUpdates(io);
+        this.io = io;
+        this.startGameUpdates();
     }
 
-    private startGameUpdates(io: Server): void {
+    private startGameUpdates(): void {
         if (this.updateInterval) {
             return;
         }
@@ -28,12 +31,16 @@ export class GameUpdateService {
         this.updateInterval = setInterval(() => {
             const now = Date.now();
             this.activeGames.forEach((game, gameId) => {
-                this.updateGame(io, gameId, game, now);
+                this.updateGame(gameId, game, now);
             });
         }, 50);
     }
 
-    private async updateGame(io: Server, gameId: string, game: Game, now: number): Promise<void> {
+    private async updateGame(
+        gameId: string,
+        game: Game,
+        now: number,
+    ): Promise<void> {
         const deltaTime = (now - game.lastUpdateTime) / 1000;
         game.lastUpdateTime = now;
 
@@ -42,21 +49,37 @@ export class GameUpdateService {
         await logic.saveGameState(this.getRedisSavers());
 
         const gameData = await getGameState(gameId);
-        io.to(gameId).emit("game_state", gameData);
-        Object.entries(ConnectionService.connections).forEach(async ([socketId, connectionData]) => {
-            const minedRes = logic.loadMinedResources(connectionData.player);
-            if (minedRes.food || minedRes.wood) {
-                const { food, wood } = connectionData.player.getResources()
-                connectionData.player.setFood(food + minedRes.food)
-                connectionData.player.setWood(wood + minedRes.wood)
-            }
-            await cachePlayerResources(connectionData.gameId, connectionData.player)
-            const data = await getPlayerCache(gameId, connectionData.playerId);
-            io.to(socketId).emit("player_state", data);
-        })
+        this.io.to(gameId).emit("game_state", gameData);
+        Object.entries(ConnectionService.connections).forEach(
+            async ([socketId, connectionData]) => {
+                const minedRes = logic.loadMinedResources(
+                    connectionData.player,
+                );
+                if (minedRes.food || minedRes.wood) {
+                    const { food, wood } = connectionData.player.getResources();
+                    connectionData.player.setFood(food + minedRes.food);
+                    connectionData.player.setWood(wood + minedRes.wood);
+                }
+                await cachePlayerResources(
+                    connectionData.gameId,
+                    connectionData.player,
+                );
+                const data = await getPlayerCache(
+                    gameId,
+                    connectionData.playerId,
+                );
+                this.io.to(socketId).emit("player_state", data);
+            },
+        );
         if (game.isGameOver()) {
+            const winner = logic.getWinner();
+            if (!winner) {
+                console.error("No winner found");
+                return;
+            }
             this.removeGame(gameId);
-            io.to(gameId).emit("game_over", { winner: logic.winnerColor });
+            this.io.to(gameId).emit("game_over", { winner });
+            await setWinnerOnGameOver(gameId, winner.getId());
             await flushGameCache(gameId);
         }
     }
@@ -78,11 +101,15 @@ export class GameUpdateService {
 
     addGame(gameId: string, game: Game): void {
         this.activeGames.set(gameId, game);
+        if (!this.updateInterval) {
+            this.startGameUpdates();
+        }
     }
 
     removeGame(gameId: string): void {
         this.activeGames.delete(gameId);
+        if (this.activeGames.size === 0) {
+            this.stopGameUpdates();
+        }
     }
-
 }
-
