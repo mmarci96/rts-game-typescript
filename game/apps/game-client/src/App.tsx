92af0a0c5b0fea06d4e9420a0ccb124c8fe81635
mmarci96 <sarosdimarci@gmail.com>
2025-03-29 11:48:00 +0100

    best i can do ...

--- game/apps/game-client/src/App.tsx
+++ game/apps/game-client/src/App.tsx
@@ -6,6 +6,7 @@ const Home = lazy(() => import("./pages/home"));
 const Games = lazy(() => import("./pages/games"));
 const Lobby = lazy(() => import("./pages/lobby"));
 const CreateGame = lazy(() => import("./pages/create-game"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route element={<Games />} path="/games" />
                 <Route element={<Lobby />} path="/lobby/:gameId" />
                 <Route element={<CreateGame />} path="/create-game" />
+                <Route path="*" element={<NotFound />} />
             </Routes>
         </Suspense>
     );
