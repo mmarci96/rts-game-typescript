8436eb41cb49de889b019d90ebb16de865495965
mmarci96 <sarosdimarci@gmail.com>
2025-03-29 04:11:06 +0100

    forgot to add the name of player to the ui

--- game/apps/game-ui/src/game/Game.ts
+++ game/apps/game-ui/src/game/Game.ts
@@ -18,6 +18,7 @@ class Game {
             player.id,
             player.color,
             gameId,
+            player.name,
         );
         this.#gameLogic = new GameLogic(assets, tiles, currentPlayer);
     }
