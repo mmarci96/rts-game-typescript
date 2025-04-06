d3639a34b24a07c4be741664b885b585ce203bd5
mmarci96 <sarosdimarci@gmail.com>
2025-04-06 20:30:37 +0200

    init

---
+++ rp-server-go/internal/configs/config.go
@@ -0,0 +1,41 @@
+package configs
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+type connection struct {
+	Name            string
+	Endpoint        string
+	Destination_URL string
+}
+
+type configuration struct {
+	Server struct {
+		Host string
+		Port string
+	}
+	Connections []connection
+}
+
+var Config *configuration
+
+func NewConfiguration() (*configuration, error) {
+	viper.AddConfigPath("data")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error loading config file: %s", err)
+	}
+	err = viper.Unmarshal(&Config)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %s", err)
+	}
+	fmt.Println("Config loaded...")
+	return Config, nil
+}
