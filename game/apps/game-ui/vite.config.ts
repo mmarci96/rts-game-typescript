33ac52d89e250badce5668c6bfedabad5fe11c43
mmarci96 <sarosdimarci@gmail.com>
2025-04-06 22:30:02 +0200

    directing the client to 3001 port to the node bridge

--- game/apps/game-ui/vite.config.ts
+++ game/apps/game-ui/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
         port: 3000,
         proxy: {
             "/socket.io": {
-                target: "http://localhost:8080",
+                target: "http://localhost:3001",
                 changeOrigin: true,
                 ws: true,
             },
