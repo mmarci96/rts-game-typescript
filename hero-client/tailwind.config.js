5e06ce1db49c0151d93c825f6acad04d987686ec
mmarci96 <sarosdimarci@gmail.com>
2025-04-13 05:58:10 +0200

    added login form:

--- hero-client/tailwind.config.js
+++ hero-client/tailwind.config.js
@@ -1,17 +1,41 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from "@heroui/theme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  darkMode: "class",
-  plugins: [heroui()],
-}
+    content: [
+        "./index.html",
+        "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--color-background)",
+                foreground: "var(--color-foreground)",
+
+                black: "var(--color-black)",
+                blue: "var(--color-blue)",
+                cyan: "var(--color-cyan)",
+                green: "var(--color-green)",
+                magenta: "var(--color-magenta)",
+                red: "var(--color-red)",
+                white: "var(--color-white)",
+                yellow: "var(--color-yellow)",
+
+                bblack: "var(--color-bblack)",
+                bblue: "var(--color-bblue)",
+                bcyan: "var(--color-bcyan)",
+                bgreen: "var(--color-bgreen)",
+                bmagenta: "var(--color-bmagenta)",
+                bred: "var(--color-bred)",
+                bwhite: "var(--color-bwhite)",
+                byellow: "var(--color-byellow)",
+            },
+        },
+    },
+    darkMode: "class",
+    plugins: [heroui()],
+};
--- hero-client/vite.config.ts
+++ hero-client/vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000",
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
+});
