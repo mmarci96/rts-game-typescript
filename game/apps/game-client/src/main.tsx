f7be0b100ae7c4c1460e47ff2d5a90a507fde0ab
mmarci96 <sarosdimarci@gmail.com>
2025-04-14 14:51:47 +0200

    game ui will be served on /game endpoint and out react app will be server on host/ , adjusted the configs and such

--- game/apps/game-client/src/main.tsx
+++ game/apps/game-client/src/main.tsx
@@ -1,7 +1,3 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
@@ -10,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <BrowserRouter basename="/game-client">
+        <BrowserRouter basename="/">
             <App />
         </BrowserRouter>
     </StrictMode>,
