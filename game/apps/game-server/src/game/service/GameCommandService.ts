6c185654d8ffabc5009837d36a44ee707da0f929
mmarci96 <sarosdimarci@gmail.com>
2025-05-08 00:34:13 +0200

    parsing commands easy as stealing candy from kindergardener

--- game/apps/game-server/src/game/service/GameCommandService.ts
+++ game/apps/game-server/src/game/service/GameCommandService.ts
@@ -1,8 +1,33 @@
 import Game from "../Game";
-import { Player } from "@packages/game-data/dist";
+import { AttackCommand, Command, MineCommand, MoveCommand, Player, TrainCommand } from "@packages/game-data/dist";
 
 export class GameCommandService {
-    handlePlayerCommands(game: Game, commands: any[], player: Player): void {
-        game.getLogic().handlePlayerCommands(commands, player);
+    handlePlayerCommands(game: Game, commands: Command[], player: Player): void {
+        console.log("Game",game);
+        console.log("Player",player);
+        
+        commands.forEach(command => {
+            switch (command.commandType) {
+                case "move":
+                    const moveCommand = command as MoveCommand;
+                    console.log("Movecommand processed: ", moveCommand);
+                    break;
+                case "train":
+                    const trainCommand = command as TrainCommand;
+                    console.log("Traincommand processed: ", trainCommand);
+                    break; 
+                case "attack":
+                    const attackCommand = command as AttackCommand;
+                    console.log("AttackCommand processed: ", attackCommand);
+                    break;
+                case "mine":
+                    const mineCommand = command as MineCommand;
+                    console.log("MineCommand processed: ", mineCommand);
+                    break; 
+                default:
+                    console.error("Invalid command: ", command);
+                    break;
+            } 
+        })
     }
 }
