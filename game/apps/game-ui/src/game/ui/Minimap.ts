d86155ec3bbceb3349e4acc9f11ea8763d6e4527
mmarci96 <sarosdimarci@gmail.com>
2025-05-08 21:31:42 +0200

    minimap view box actually centered, wooooOOu

--- game/apps/game-ui/src/game/ui/Minimap.ts
+++ game/apps/game-ui/src/game/ui/Minimap.ts
@@ -26,31 +26,40 @@ class Minimap {
         const cameraY = this.camera.getY();
         const cameraWidth = this.camera.getWidth();
         const cameraHeight = this.camera.getHeight();
-        console.log("Camera location, x: ", cameraX, ", y: ", cameraY);
-        console.log("Height: ", cameraHeight, ", Width: ", cameraWidth);
 
         const ctx = this.canvas.getContext("2d");
         if (!ctx) {
             throw new Error("Failed to get 2D rendering context for canvas");
         }
-        const tileSize = 156 / this.tiles.length;
+
+        const mapWidth = this.tiles[0].length;
+        const mapHeight = this.tiles.length;
+        const tileWidth = this.canvas.width / mapWidth;
+        const tileHeight = this.canvas.height / mapHeight;
+
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        for (let y = 0; y < this.tiles.length; y++) {
+
+        for (let y = 0; y < mapHeight; y++) {
             const row = this.tiles[y];
-            for (let x = 0; x < row.length; x++) {
+            for (let x = 0; x < mapWidth; x++) {
                 const tname = row[x].tile;
                 ctx.fillStyle = tname === "water1" ? "blue" : "green";
-                ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+                ctx.fillRect(
+                    x * tileWidth,
+                    y * tileHeight,
+                    tileWidth,
+                    tileHeight,
+                );
             }
         }
 
-        ctx.strokeStyle = "white"; // Set the color for the border
-        ctx.lineWidth = 2; // (optional) Make the line a bit thicker if you want
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 2;
         ctx.strokeRect(
-            tileSize * cameraX,
-            tileSize * cameraY,
-            cameraWidth * tileSize,
-            cameraHeight * tileSize,
+            cameraX * tileWidth - (cameraWidth * tileWidth) / 2,
+            cameraY * tileHeight - (cameraHeight * tileHeight) / 2,
+            cameraWidth * tileWidth,
+            cameraHeight * tileHeight,
         );
     }
 }
