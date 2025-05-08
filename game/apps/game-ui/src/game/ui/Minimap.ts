6883f967ff06f436351e67e0fc2abd693e443b25
mmarci96 <sarosdimarci@gmail.com>
2025-05-08 14:24:34 +0200

    minimap color

--- game/apps/game-ui/src/game/ui/Minimap.ts
+++ game/apps/game-ui/src/game/ui/Minimap.ts
@@ -25,9 +25,11 @@ class Minimap {
         for (let y = 0; y < this.tiles.length; y++) {
             const row = this.tiles[y];
             for (let x = 0; x < row.length; x++) {
+                console.log(row[x]);
+
                 const tileSize = 156 / row.length;
                 const tname = row[x].tile;
-                ctx.fillStyle = tname === "water1" ? "blue" : "brown";
+                ctx.fillStyle = tname === "water1" ? "blue" : "green";
                 ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
             }
         }
