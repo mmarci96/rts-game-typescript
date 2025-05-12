c5346db943d73c68d4a600d55ec94ddad8d12101
mmarci96 <sarosdimarci@gmail.com>
2025-05-12 15:56:01 +0200

    calling watcher

--- proxy-server/cmd/main.go
+++ proxy-server/cmd/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"log"
 
-	"github.com/mmarci96/rts-game-monorepo/proxy-server/internal/kubernetes"
 	"github.com/mmarci96/rts-game-monorepo/proxy-server/internal/server"
+	"github.com/mmarci96/rts-game-monorepo/proxy-server/internal/watcher"
 )
 
 func main() {
-	go kubernetes.WatchEndpointSlices()
+	go watcher.WatchEndpointSlices()
 
 	err := server.Run()
 	if err != nil {
