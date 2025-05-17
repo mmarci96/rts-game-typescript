3868f06bc3c06e638fece8fa4f142d35362f8026
mmarci96 <sarosdimarci@gmail.com>
2025-05-17 23:50:55 +0200

    hero client status set ready button added

--- hero-client/src/components/player-card.tsx
+++ hero-client/src/components/player-card.tsx
@@ -1,11 +1,43 @@
 import { Player } from "@/types";
 import { RxAvatar } from "react-icons/rx";
-import { Card, CardHeader, CardBody, CardFooter, Divider } from "@heroui/react";
+import { useUserContext } from "@/context/user-context";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Divider,
+    Button,
+} from "@heroui/react";
 interface PlayerCardProps {
     player: Player;
 }
 export const PlayerCard = ({ player }: PlayerCardProps) => {
-    console.log(player);
+    const { userData } = useUserContext();
+    const handleReady = async () => {
+        if (player.userId !== userData.id) {
+            return;
+        }
+        const res = await fetch("/api/players/" + player._id, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ isReady: true }),
+        });
+        const data = await res.json();
+        console.log("Ready: ", data);
+    };
+    const handleLeave = async () => {
+        if (player.userId !== userData.id) {
+            return;
+        }
+        const res = await fetch("/api/players/" + player._id, {
+            method: "DELETE",
+        });
+        const data = await res.json();
+        console.log("Left: ", data);
+    };
 
     return (
         <Card className="max-w-[400px] p-2 m-4 min-w-[320px]">
@@ -21,6 +53,20 @@ export const PlayerCard = ({ player }: PlayerCardProps) => {
             <Divider />
             <CardBody>
                 <p>Status: {player.isReady ? "Ready" : "Waiting"}</p>
+                {userData.id === player.userId && (
+                    <div>
+                        <Button onPress={handleReady} variant="flat">
+                            Ready
+                        </Button>
+                        <Button
+                            onPress={handleLeave}
+                            variant="flat"
+                            color="warning"
+                        >
+                            Leave
+                        </Button>
+                    </div>
+                )}
             </CardBody>
             <Divider />
             <CardFooter>
--- hero-client/src/pages/lobby.tsx
+++ hero-client/src/pages/lobby.tsx
@@ -1,28 +1,56 @@
 import { JoinedPlayers } from "@/components/joined-players";
 import { title } from "@/components/primitives";
+import { useUserContext } from "@/context/user-context";
 import { useApiRequest } from "@/hooks/use-apirequest";
 import DefaultLayout from "@/layouts/default";
-import { HttpMethod, Player } from "@/types";
+import { GameData, HttpMethod, Player } from "@/types";
+import { Button } from "@heroui/button";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function LobbyPage() {
     const [joinedPlayers, setJoinedPlayers] = useState<Player[]>([]);
+    const [gameData, setGameData] = useState<GameData | null>(null);
+    const [startable, setStartable] = useState(false);
     const { gameId } = useParams();
     const { fetchApiData } = useApiRequest();
+    const { userData } = useUserContext();
     const fetchGameData = async (gameId: string) => {
-        const { game, players } = await fetchApiData(
-            "/api/games/" + gameId,
-            HttpMethod.GET,
-            null,
-        );
+        const { game, players }: { game: GameData; players: Player[] } =
+            await fetchApiData("/api/games/" + gameId, HttpMethod.GET, null);
+        if (!game && !players) {
+            return;
+        }
         console.log("Game data: ", game);
         console.log("Players: ", players);
-        setJoinedPlayers(players);
+
+        if (game?.status === "ready") {
+            console.log("READY");
+            //Redirect to game with playerid and gameid
+            const player = players.find(
+                (p: Player) => p.userId === userData.id,
+            );
+            window.location.href = "/game/" + gameId + player?._id;
+        }
+        if (players.length !== joinedPlayers.length) {
+            setJoinedPlayers(players);
+            setGameData(game);
+        }
+        if (players.length === game.maxPlayers) {
+            setStartable(true);
+        }
     };
+
+    const handleStart = async () => {
+        const url = "/api/games/start/" + gameId;
+        await fetchApiData(url, HttpMethod.PATCH, null);
+    };
+
     useEffect(() => {
-        console.log(gameId);
-        gameId && fetchGameData(gameId);
+        !gameData && gameId && fetchGameData(gameId);
+        setInterval(() => {
+            gameId && fetchGameData(gameId);
+        }, 3000);
     }, [gameId]);
 
     return (
@@ -39,7 +67,12 @@ export default function LobbyPage() {
                     <h1 className={title()}>Lobby</h1>
                 </div>
                 {joinedPlayers ? (
-                    <JoinedPlayers players={joinedPlayers} />
+                    <>
+                        <JoinedPlayers players={joinedPlayers} />
+                        {startable && (
+                            <Button onPress={handleStart}>Start</Button>
+                        )}
+                    </>
                 ) : (
                     <p>loading..</p>
                 )}
