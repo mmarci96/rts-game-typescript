b1c63eceed1b9e4dfdc320693815b2a5d1e8c2ba
mmarci96 <sarosdimarci@gmail.com>
2025-05-17 18:27:03 +0200

    logging in user

--- hero-client/src/components/auth-form.tsx
+++ hero-client/src/components/auth-form.tsx
@@ -3,6 +3,10 @@ import { Form } from "@heroui/form";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
 import { Tabs, Tab } from "@heroui/tabs";
+import { useUserContext } from "@/context/user-context";
+import { Chip } from "@heroui/react";
+import { RiErrorWarningFill } from "react-icons/ri";
+import { UserData } from "@/types";
 
 export const AuthForm = () => {
     const [formFields, setFormFields] = useState({
@@ -10,10 +14,12 @@ export const AuthForm = () => {
         email: "",
         password: "",
     });
-    const [errors, setErrors] = useState({});
+    const [error, setError] = useState<string | null>(null);
     const [selected, setSelected] = useState("login");
+    const { userData, onLogin } = useUserContext();
 
     const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+        setError(null);
         const { name, value } = e.currentTarget;
         setFormFields((prev) => ({
             ...prev,
@@ -33,38 +39,41 @@ export const AuthForm = () => {
         });
 
         if (!response.ok) {
-            console.error(response.statusText);
-            setErrors({
-                form: `Error with ${selected} request response.`,
-            });
+            const err = await response.json();
+            setError(err.message);
             return;
         }
 
-        const { data } = await response.json();
-        localStorage.setItem("userId", data.id);
+        const { data }: { data: UserData } = await response.json();
+        onLogin(data);
         console.log(`Request ${selected} response:  `, data);
     };
 
-    return (
+    return userData.id ? (
+        <p>Already logged in</p>
+    ) : (
         <Form
             className="w-full mx-auto max-w-sm flex flex-col items-center ring-1 ring-secondary rounded-xl p-8"
-            validationErrors={errors}
             onSubmit={handleSubmit}
         >
             <Tabs
                 aria-label="Login or Sing up to play!"
                 selectedKey={selected}
                 size="md"
-                onSelectionChange={(e) => setSelected(e.toString())}
+                onSelectionChange={(e) => {
+                    setSelected(e.toString());
+                    setError(null);
+                }}
             >
                 <Tab key={"login"} title="Login">
                     <div className="w-full max-w-md flex flex-col items-center gap-4">
                         <Input
-                            label="Username"
+                            label="Email"
                             labelPlacement="outside"
-                            name="username"
-                            value={formFields.username}
-                            placeholder="Enter your username"
+                            name="email"
+                            type="email"
+                            value={formFields.email}
+                            placeholder="Enter your email"
                             onChange={handleChange}
                         />
                         <Input
@@ -125,6 +134,18 @@ export const AuthForm = () => {
                     </div>
                 </Tab>
             </Tabs>
+            {error && (
+                <span className="flex">
+                    <RiErrorWarningFill className="text-warning-500 text-xl my-1" />
+                    <Chip
+                        variant="light"
+                        color="warning"
+                        className="text-md mt-0"
+                    >
+                        {error}
+                    </Chip>
+                </span>
+            )}
         </Form>
     );
 };
