d21ae4edb0ef0ecdd1b65ff739612c338ef4eab4
mmarci96 <sarosdimarci@gmail.com>
2025-05-17 18:26:37 +0200

    provide the usercontext

--- hero-client/src/provider.tsx
+++ hero-client/src/provider.tsx
@@ -2,19 +2,22 @@ import type { NavigateOptions } from "react-router-dom";
 
 import { HeroUIProvider } from "@heroui/system";
 import { useHref, useNavigate } from "react-router-dom";
+import { UserContextProvider } from "./context/user-context";
 
 declare module "@react-types/shared" {
-  interface RouterConfig {
-    routerOptions: NavigateOptions;
-  }
+    interface RouterConfig {
+        routerOptions: NavigateOptions;
+    }
 }
 
 export function Provider({ children }: { children: React.ReactNode }) {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <HeroUIProvider navigate={navigate} useHref={useHref}>
-      {children}
-    </HeroUIProvider>
-  );
+    return (
+        <UserContextProvider>
+            <HeroUIProvider navigate={navigate} useHref={useHref}>
+                {children}
+            </HeroUIProvider>
+        </UserContextProvider>
+    );
 }
