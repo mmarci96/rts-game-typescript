7dfe4763e83da464954616e98d9b8b0adb514a84
mmarci96 <sarosdimarci@gmail.com>
2025-05-17 19:19:14 +0200

    fixed icon dissappear

--- hero-client/src/config/site.ts
+++ hero-client/src/config/site.ts
@@ -1,9 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-    name: "Vite + HeroUI",
-    description:
-        "Make beautiful websites regardless of your design experience.",
+    name: "RTS game",
+    description: "Create or find a game online",
     navItems: [
         {
             label: "Home",
